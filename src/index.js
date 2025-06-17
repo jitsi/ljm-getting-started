6ce94db76bc2f0083ee524708ea9f08329bb64d5
Hugo Lavernhe <hugo.lavernhe@gmail.com>
2025-06-17 19:48:22 +0200

    feat: Add audio settings checkboxes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const state = {
     room: '',
     jwt: '',
     conference: undefined,
+    localAudioTrack: undefined
 };
 
 // Form elements.
@@ -16,6 +17,7 @@ const roomEl = document.getElementById('roomText');
 const jwtEl = document.getElementById('jwtText');
 const joinBtn = document.getElementById('joinBtn');
 const leaveBtn = document.getElementById('leaveBtn');
+const audioSettingsForm = document.getElementById('audioSetings');
 
 function updateJoinForm() {
     // In a meeting.
@@ -63,29 +65,46 @@ leaveBtn.onclick = async () => {
 
 
 const handleTrackAdded = track => {
-    if (track.getType() === 'video') {
-        const meetingGrid = document.getElementById('meeting-grid');
-        const videoNode = document.createElement('video');
+	if (track.getType() === 'video') {
+		const meetingGrid = document.getElementById('meeting-grid');
+		const videoNode = document.createElement('video');
 
-        videoNode.id = track.getId();
-        videoNode.className = 'jitsiTrack col-4 p-1';
-        videoNode.autoplay = true;
-        meetingGrid.appendChild(videoNode);
-        track.attach(videoNode);
-    } else if (!track.isLocal()) {
-        const audioNode = document.createElement('audio');
+		videoNode.id = track.getId();
+		videoNode.className = 'jitsiTrack col-4 p-1';
+		videoNode.autoplay = true;
+		meetingGrid.appendChild(videoNode);
+		track.attach(videoNode);
+	} else {
+        if (track.isLocal()) {                  
+            const checkboxes = audioSettingsForm.querySelectorAll('.form-check-input');
+            const audioTrackSettings = track.getTrack().getSettings();
+ 
+            checkboxes.forEach((cb) => {
+                cb.checked = cb.id === 'channelCount' ? audioTrackSettings[cb.id] === 2 : audioTrackSettings[cb.id];   
+            }) 
+        
+            state.localAudioTrack = track;
+            audioSettingsForm.style.visibility = 'visible';
+		} else {
+			const audioNode = document.createElement('audio');
 
-        audioNode.id = track.getId();
-        audioNode.className = 'jitsiTrack';
-        audioNode.autoplay = true;
-        document.body.appendChild(audioNode);
-        track.attach(audioNode);
-    }
+			audioNode.id = track.getId();
+			audioNode.className = 'jitsiTrack';
+			audioNode.autoplay = true;
+			document.body.appendChild(audioNode);
+			track.attach(audioNode);
+		}
+	}
 };
 
 const handleTrackRemoved = track => {
     track.dispose();
     document.getElementById(track.getId())?.remove();
+
+    if (track.getTrack().id === state.localAudioTrack.getTrack().id) {
+        state.localAudioTrack = null;
+        audioSettingsForm.style.visibility = 'hidden';
+    }
 };
 
 const onConferenceJoined = () => {
@@ -142,6 +161,28 @@ async function leave() {
 
     state.conference = undefined;
 }
+
+async function handleBtnSettings() {
+    const checkboxes = audioSettingsForm.querySelectorAll('.form-check-input');
+    const audioSettings = {};
+    
+    checkboxes.forEach(cb => {
+        audioSettings[cb.id] = cb.id === 'channelCount' ? (cb.checked ? 2 : 1) : cb.checked;
+    });
+
+    try {
+        await state.localAudioTrack.applyConstraints(audioSettings);
+    } catch(err) {
+        console.error(err);
+    }   
+    
+    const audioTrackSettings = state.localAudioTrack.getTrack().getSettings();
+
+    checkboxes.forEach((cb) => {
+        cb.checked = cb.id === 'channelCount' ? audioTrackSettings[cb.id] === 2 : audioTrackSettings[cb.id];   
+    }) 
+}
+document.getElementById('btnSettings').addEventListener('click', handleBtnSettings);
 
 // Initialize library.
 JitsiMeetJS.init();
